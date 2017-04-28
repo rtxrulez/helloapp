6d720afbcc7037cc44d29b48d6da49323def064f
Ramil <vafarulez@ya.ru>
2017-04-28 23:40:47 +0300

    router

--- script3.js
+++ script3.js
@@ -1,42 +1,44 @@
-class nameField extends React.Component {
-    constructor(props) {
-        super(props);
-        var isValid  = this.validate(props.value);
-        this.state = {value: props.value, valid: isValid}
-        this.onChange = this.onChange.bind(this);
-    }
-    onChange(e) {
-        var val = e.target.value;
-        var isValid = this.validate(val);
-        this.setState({value: val, valid: isValid});
-    }
-    render() {
-        // если прошла валидация то норм
-        var color = this.state.valid === true ? "green" : "red";
-        return <p>
-            <label>Имя: </label> <br/>
-            <input type="text" value={this.state.value} onChange={this.onChange} style={{borderColor: color}}></input>
-        </p>
-    }
-}
-class UserFrom extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleSubmit(e) {
-        e.preventDefault();
-        console.log('test', this.refs.nameField.value);
-    }
-    render() {
-        return <form onSubmit={this.handleSubmit}>
-            <input ref="nameField"></input>
-            <input type="submit" value="Отправить"></input>
-        </form>
-    }
-}
+var Router = ReactRouter.Router;
+    var Route = ReactRouter.Route;
+    var IndexRoute = ReactRouter.IndexRoute;
+    var browserHistory = ReactRouter.browserHistory;
 
-ReactDOM.render(
-    <UserFrom />,
-    document.getElementById('container')
-)
+    class About extends React.Component{
+        render(){
+            return <h2>О сайте</h2>;
+        }
+    }
+    class NotFound extends React.Component{
+        render(){
+            return <h2>Ресурс не найден</h2>;
+        }
+    }
+    class Products extends React.Component{
+        render(){
+            return <h2>Товары</h2>;
+        }
+    }
+    class Home extends React.Component{
+        render(){
+            return <h2>Главная</h2>;
+        }
+    }
+    class Main extends React.Component{
+        render(){
+            return <div>
+                    <h2>Добро пожаловать на сайт</h2>
+                    {this.props.children}
+                   </div>;;
+        }
+    }
+    ReactDOM.render(
+        <Router history={browserHistory}>
+            <Route path="/" component={Main}>
+                <Route path="about" component={About} />
+                <Route path="products" component={Products} />
+                <IndexRoute component={Home} />
+            </Route>
+            <Route path="*" component={NotFound} />
+        </Router>,
+        document.getElementById("container")
+    )
